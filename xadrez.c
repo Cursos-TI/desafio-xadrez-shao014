26bb7a3e697c03a7b562faecc00c0f281deaa017
shao014 <andrelu014@gmail.com>
2025-06-05 12:54:32 +0000

    alteracoes

--- xadrez.c
+++ xadrez.c
@@ -2,26 +2,27 @@
 
 
 int main() {
-    
+
+   //mover a torre 5 casas para a direita.
    int torre, bispo, rainha;
 
    for ( torre = 0; torre < 5; torre++)
    {
-    printf("Direta\n");
+    printf("Direta\n"); // imprime a direção do movimento.
    }
    
+   //mover o bispo 5 casas para a diagonal na direção direita e para cima.
    while (bispo < 5)
    {
-    printf("diagonal: Cima e direta\n");
+    printf("diagonal: Cima e direta\n"); // imprime a direção do movimento.
     bispo++;
    }
    
+   // mover a rainha 8 casas para a esquerda.
    do {
-    printf("Esquerda\n");
+    printf("Esquerda\n"); //impreme a direção do movimento.
     rainha++;
    } while (rainha <8);
-
-    printf("Fim do jogo!");
 
     return 0;
 }
